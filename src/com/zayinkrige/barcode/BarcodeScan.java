6caab184c277fbcf528cda69b1ae8b8535e5cd43
Zayin Krige <zkrige@gmail.com>
2014-05-11 09:13:16 +0200

    Initial Commit

---
+++ src/com/zayinkrige/barcode/BarcodeScan.java
@@ -0,0 +1,131 @@
+package com.zayinkrige.barcode;
+/*
+ * Based on ZBar AndroidSDK samples. 
+ * 
+ * Converted to an activity that returns a result 
+ */
+
+
+import com.zayinkrige.barcode.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.FrameLayout;
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
+import android.hardware.Camera.AutoFocusCallback;
+import android.hardware.Camera.Size;
+
+/* Import ZBar Class files */
+import net.sourceforge.zbar.ImageScanner;
+import net.sourceforge.zbar.Image;
+import net.sourceforge.zbar.Symbol;
+import net.sourceforge.zbar.SymbolSet;
+import net.sourceforge.zbar.Config;
+
+public class BarcodeScan extends Activity {
+	private Camera mCamera;
+	private CameraPreview mPreview;
+	private Handler autoFocusHandler;
+
+	ImageScanner scanner;
+
+	private boolean previewing = true;
+
+	static {
+		System.loadLibrary("iconv");
+	}
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.barcodescan);
+
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+		autoFocusHandler = new Handler();
+		mCamera = getCameraInstance();
+
+		/* Instance barcode scanner */
+		scanner = new ImageScanner();
+		scanner.setConfig(0, Config.X_DENSITY, 3);
+		scanner.setConfig(0, Config.Y_DENSITY, 3);
+
+		mPreview = new CameraPreview(this, mCamera, previewCb, autoFocusCB);
+		FrameLayout preview = (FrameLayout) findViewById(R.id.cameraPreview);
+		preview.addView(mPreview);
+
+	}
+
+	public void onPause() {
+		super.onPause();
+		releaseCamera();
+	}
+
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open();
+		} catch (Exception e) {
+		}
+		return c;
+	}
+
+	private void releaseCamera() {
+		if (mCamera != null) {
+			previewing = false;
+			mCamera.setPreviewCallback(null);
+			mCamera.release();
+			mCamera = null;
+		}
+	}
+
+	private Runnable doAutoFocus = new Runnable() {
+		public void run() {
+			if (previewing)
+				mCamera.autoFocus(autoFocusCB);
+		}
+	};
+
+	PreviewCallback previewCb = new PreviewCallback() {
+		public void onPreviewFrame(byte[] data, Camera camera) {
+			Camera.Parameters parameters = camera.getParameters();
+			Size size = parameters.getPreviewSize();
+
+			Image barcode = new Image(size.width, size.height, "Y800");
+			barcode.setData(data);
+
+			int result = scanner.scanImage(barcode);
+
+			if (result != 0) {
+				previewing = false;
+				mCamera.setPreviewCallback(null);
+				mCamera.stopPreview();
+
+				SymbolSet syms = scanner.getResults();
+				for (Symbol sym : syms) {
+					Intent intent = new Intent();
+					intent.putExtra("barcode", sym.getData());
+					if (getParent() == null) {
+						setResult(Activity.RESULT_OK, intent);
+					} else {
+						getParent().setResult(Activity.RESULT_OK, intent);
+					}
+					finish();
+				}
+			}
+		}
+	};
+
+	// Mimic continuous auto-focusing
+	AutoFocusCallback autoFocusCB = new AutoFocusCallback() {
+		public void onAutoFocus(boolean success, Camera camera) {
+			//autofocus 200ms
+			autoFocusHandler.postDelayed(doAutoFocus, 200);
+		}
+	};
+}
